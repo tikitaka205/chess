3a3deead3ee71d1622e13559820a5eb7e6e7f6e1
minsu <splendent77@gmail.com>
2024-01-20 16:35:46 +0900

    rook_move

--- chess/views.py
+++ chess/views.py
@@ -109,6 +109,7 @@ class Chess:
     #조건을 만족하면 움직일 것이다
     #들어오는 조건을 계속 생각해보자
     #반복문으로 장애물 확인
+    #+상대방 공격
     def move_pawn(self, from_positon, to_position):
         if from_positon[:-2] ==to_position:
             return False
@@ -121,11 +122,11 @@ class Chess:
         print("move_pawn", i_from, j_from, i_to, j_to) #여기서 i j 는 실제 0,0을 의미
         if isValid:
             dif=i_to-i_from
-            print("dif",dif)
+            # print("dif",dif)
             if from_positon[-2]==BLACK:
                 print("black")
                 if dif >= 1 and dif <= 2: #if=1 or 2 로 해도 될듯?
-                    print("1,2칸 움직임")
+                    # print("1,2칸 움직임")
                     #앞에 말이 막고있는지 확인
                     # is_okay=True
                     for i in range(1, dif+1): #항상 그냥 숫자만 적지말고 연관된 숫자를 활용하자
@@ -146,20 +147,127 @@ class Chess:
                 print("WHITE")
                 if dif >= -2 and dif <= -1:
                     for i in range(1, abs(dif)+1):
-                        if self.board[i_from + i][j_from]!=EMPTY:
+                        if self.board[i_from - i][j_from]!=EMPTY:
                             return False
                         else:
                             self.board[i_from][j_from]=EMPTY
-                            self.board[i_to][j_to]=from_positon[0] + PAWN
+                            self.board[i_to][j_to]=from_positon[2] + PAWN
+                            print("white움직임")
                             return True
                 else:
                     return False
         else:
             return False
 
+    #TODO
+    #okay 종 횡으로 움직임
+    #okay 같은자리 움직임 금지
+    #okay 체스판 안의 범위로 이동
+    #okay 말이 없는 자리를 입력하면 말이 생김
+    #okay 말이 가는 포지션까지 다른말이 있으면 안된다
+    #공격하는 말이 나의 말이면 안된다
+    #움직이는 곳에 상대말이 있다면 제거
+    #룩은 위아래오른쪽왼쪽 다 움직일 수 있다
+
+    #move_rook
+    #move_rook('a7bR','a6')
+    def move_rook(self, from_positon, to_position):
+        # isValid_from, i_from, j_from=self.transform_str_to_num(from_positon[:2])
+        # isValid_to, i_to, j_to=self.transform_str_to_num(to_position)
+        # isJPositionSame=j_from==j_to
+        # isIPositionSame=i_from!=i_to
+
+        isValid_from, i_from, j_from=self.transform_str_to_num(from_positon[:2])
+        isValid_to, i_to, j_to=self.transform_str_to_num(to_position)
+        isVaild_same_positon = i_from!=i_to and j_from!=j_to
+        isVaild_horizon=i_from == i_to and j_from != j_to
+        isVaild_vertical=i_from != i_to and j_from == j_to
+        isVaild_is=self.board[i_from][j_from]!=EMPTY
+        isVaild=isValid_from and isValid_to and isVaild_is
+        if isVaild:
+            # is_ok=True
+            # is_kill=False
+            # print(i_from)
+            # print(j_from)
+            if isVaild_horizon:
+                print("호리즌",isVaild_horizon)
+                dif=j_to-j_from
+                for i in range(1,abs(dif)+1):
+                    # i_from이 a1 이면 i,j는 0부터 해서 i=7 j=0이 된다
+                    #여기서 조건이 되면 + 된다면 위, 오른쪽 - 되면 아래, 왼쪽
+                    #위로 움직이면 i_dif가 -된다
+                    #6-8 =-2움직임 = 위
+                    #8-6 = 2움직임 = 아래
+                    if i < dif:
+                        if self.board[i_from][j_from+i] != EMPTY:
+                            print("horizon 움직임 실패")
+                            return False
+                        else:
+                            self.board[i_from][j_from]=EMPTY
+                            self.board[i_to][j_to] = from_positon[2] + ROOK
+                            print("horizon 움직임 성공")
+                            return True
+                    else:
+                        if self.board[i_from][j_from+i] != EMPTY:
+                            print("horizon 움직임 실패")
+                            return False
+                        else:
+                            self.board[i_from][j_from]=EMPTY
+                            self.board[i_to][j_to] = from_positon[2] + ROOK
+                            print("horizon 움직임 성공")
+                            return True
+
+            elif isVaild_vertical:
+                print("버티컬",isVaild_vertical)
+                dif=i_to-i_from
+                for i in range(1,abs(dif)+1):
+                    #룩이 위로움직냐 아래로 움직이냐
+                    #룩 위로 움직임
+                    if i > dif:
+
+                        print(i_from)
+                        print(i_to)
+                        print(dif)
+                        if self.board[i_from-i][j_from] != EMPTY:
+                            #5에서 0으로 이동 dif=-5
+                            #0-7범위 여기범위에서는
+                            #b3 b8이 뚫린다 범위 체크다시 확인
+                            #5->0
+                            print("i",i)
+                            print("vertical 위 움직임 실패")
+                            break
+                        else:
+                            print("i",i)
+                            self.board[i_from][j_from]=EMPTY
+                            self.board[i_to][j_to] = from_positon[2] + ROOK
+                            print("vertical 위 움직임 성공")
+                            return True
+                    #룩 아래로 움직임  
+                    else:
+                        if self.board[i_from+i][j_from] != EMPTY:
+                            print("vertical 아래 움직임 실패")
+                            return False
+                        else:
+                            self.board[i_from][j_from]=EMPTY
+                            self.board[i_to][j_to] = from_positon[2] + ROOK
+                            print("vertical 아래 움직임 성공")
+                            return True
+
+            print("vaild는 성공 버티컬 호리즌으로 안들어감")
+            return True
+        else:
+            print("룩 vaild 실패")
+            return False
+
 chess=Chess()
 chess.set_game() #=Chess().print_board() 체스 클래스에 print_board()라는 함수를 실행
-chess.move_pawn('a7bP','a5')
+chess.move_pawn('a7bP','a5')#폰 아래로 이동
+chess.move_pawn('a2wP','a4')#폰 위로이동
+chess.move_rook('a1wR','a3')#룩 위로이동
+chess.move_rook('a3wR','d3')#룩 오른쪽 이동
+chess.move_rook('d3wR','b3')#룩 왼쪽이동
+chess.move_rook('b3wR','b8'),print("말을 넘어가")#말을 넘어가는 에러
+chess.move_rook('b8wR','b3')#룩 아래이동
 chess.print_board()
 
 
